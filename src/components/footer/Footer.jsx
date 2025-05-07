31fdc51605d902a4533e80fb92cf934482ffda15
mrifaldi25 <rifaldidi2025>
2025-05-07 19:42:49 +0700

    lanjutan dhavi

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+import "./footer.scss";
+
+import { Link } from "react-router-dom";
+
+import bg from "./../../assets/footer-bg.jpg";
+import logo from "./../../assets/Logo_2.png";
+
+import * as Config from "./../../constants/Config";
+
+const Footer = () => {
+  return (
+    <div className="footer" style={{ backgroundImage: `url(${bg})` }}>
+      <div className="footer__content container">
+        <div className="footer__content__logo">
+          <div className="Logo_2.png">
+            <img src={logo} alt="Logo_2.png" />
+            <Link to={`/${Config.HOME_PAGE}`}>@MovieVerse2025</Link>
+          </div>
+        </div>
+
+        <div className="footer__content__menus">
+          <div className="footer__content__menu">
+            <Link to={`/${Config.HOME_PAGE}`}>Home</Link>
+            <Link to={`/${Config.HOME_PAGE}`}>Contact us</Link>
+            <Link to={`/${Config.HOME_PAGE}`}>Term of service</Link>
+            <Link to={`/${Config.HOME_PAGE}`}>About us</Link>
+          </div>
+          <div className="footer__content__menu">
+            <Link to={`/${Config.HOME_PAGE}`}>Live</Link>
+            <Link to={`/${Config.HOME_PAGE}`}>FAQ</Link>
+            <Link to={`/${Config.HOME_PAGE}`}>Premium</Link>
+            <Link to={`/${Config.HOME_PAGE}`}>Privacy policy</Link>
+          </div>
+          <div className="footer__content__menu">
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
